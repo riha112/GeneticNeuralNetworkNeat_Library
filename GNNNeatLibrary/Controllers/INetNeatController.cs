6da86c28ecfbb6a328e0dccb0d5af3815b610ace
Rihards Abo <denkfrt@inbox.lv>
2020-03-18 17:29:08 +0200

    Added NetNeatController with FeedForward and DI support Created new interface to support DI. Added two helper functions: FindNode and GetInnovationRange

---
+++ GNNNeatLibrary/Controllers/INetNeatController.cs
@@ -0,0 +1,16 @@
+﻿using DataManager.Models;
+
+namespace GNNNeatLibrary.Controllers
+{
+    public interface INetNeatController
+    {
+        void FeedForward(ref NetModel target, double[] input);
+
+        /// <summary>
+        /// Finds networks innovation ranges min max
+        /// </summary>
+        /// <param name="net">Searchable network</param>
+        /// <returns>tuple of minimum innovations id and maximum innovations id</returns>
+        (int min, int max) GetInnovationRange(NetModel net);
+    }
+}
