2d88ad1002185ae279b257e9fc013bc5b14c9cda
Rihards Abo <denkfrt@inbox.lv>
2020-03-18 17:31:00 +0200

    Added InnovationController with DB and DI support. Created two classes InnovationLocalController that works locally without interference with DB (not yet implemented) AND InnovationDbController that searches for innovation inside DB Also added interference for DI support

---
+++ GNNNeatLibrary/Controllers/InnovationDbController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataManager.Models;
+using DataManager.Processors;
+
+namespace GNNNeatLibrary.Controllers
+{
+    public class InnovationDbController : IInnovationController
+    {
+        private readonly IInnovationProcessor _innovationProcessor;
+
+        public InnovationDbController(IInnovationProcessor innovationProcessor) =>
+            _innovationProcessor = innovationProcessor;
+
+        /// <summary>
+        /// If innovation exists in database then it's returned, otherwise
+        /// new innovation is created and returned. 
+        /// </summary>
+        /// <param name="innovation">Fetch-able innovation</param>
+        /// <returns>Linked innovation</returns>
+        public InnovationModel FetchWithInsertOnFail(InnovationModel innovation)
+        {
+            try
+            {
+                // Searches for innovation
+                var output = _innovationProcessor.Find(
+                    innovation.Type, 
+                    innovation.NodeFromId, 
+                    innovation.NodeToId, 
+                    innovation.BatchId);
+                return output;
+            }
+            catch (Exception)   // TODO: Make custom Exception for element not found
+            {
+                // If innovation is not found then save it
+                _innovationProcessor.Save(ref innovation);  // Saves and Updates Id
+                return innovation;
+            }
+        }
+    }
+}
---
+++ GNNNeatLibrary/Controllers/InnovationLocalController.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DataManager.Models;
+
+namespace GNNNeatLibrary.Controllers
+{
+    class InnovationLocalController : IInnovationController
+    {
+        public InnovationModel FetchWithInsertOnFail(InnovationModel innovation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
