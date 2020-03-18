56fc83abb75ed1061de8867b35a4469e8cbaa012
Rihards Abo <denkfrt@inbox.lv>
2020-03-18 17:35:03 +0200

    Updated DI container

--- TicTacToeConsoleUI/ContainerConfig.cs
+++ TicTacToeConsoleUI/ContainerConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using Autofac;
+using DataManager.Utilities;
 
 namespace TicTacToeConsoleUI
 {
@@ -18,11 +19,13 @@ namespace TicTacToeConsoleUI
             
             // Registers dependencies:
             builder.RegisterType<Application>().As<IApplication>();
+            builder.RegisterType<SqlDataAccess>().As<ISqlDataAccess>();
 
-            // Initializes DataManage/Models
-            //builder.RegisterAssemblyTypes(Assembly.Load(nameof(DataManager)))
-            //    .Where(t => t.Namespace.Contains("Models"))
-            //    .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
+            // Initializes DataManager/Processors
+            builder.RegisterAssemblyTypes(Assembly.Load(nameof(DataManager)))
+                .Where(t => t.Namespace.Contains("Processors"))
+                .As(t => t.GetInterfaces().FirstOrDefault(i => i.Name == "I" + t.Name));
+
 
 
             return builder.Build();
