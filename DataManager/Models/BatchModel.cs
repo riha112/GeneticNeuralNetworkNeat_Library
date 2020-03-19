9f3ae30080f9d333cf3dfd0b7e6fa5b74dd941df
Rihards Abo <denkfrt@inbox.lv>
2020-03-19 18:31:49 +0200

    Added field Generation

--- DataManager/Models/BatchModel.cs
+++ DataManager/Models/BatchModel.cs
@@ -13,6 +13,8 @@ namespace DataManager.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int Generation { get; set; }
+
         public DateTime CreationDate { get; set; }
 
         public List<NetModel> Networks { get; set; } = new List<NetModel>();
