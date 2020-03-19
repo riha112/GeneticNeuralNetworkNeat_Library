8f7e202963ed748c7355700effc24d1ea3a6241c
Rihards Abo <denkfrt@inbox.lv>
2020-03-19 18:37:22 +0200

    Done with spiecies controller

---
+++ GNNNeatLibrary/Controllers/Species/ISpeciesController.cs
@@ -0,0 +1,23 @@
+﻿using DataManager.Models;
+using GNNNeatLibrary.Model;
+
+namespace GNNNeatLibrary.Controllers.Species
+{
+    public interface ISpeciesController
+    {
+        SpeciesModel GetNew();
+        SpeciesModel GetNewWithHead(NetModel headNetModel);
+        void CalculateAverageScore(SpeciesModel speciesModel);
+        void SortByFitnessScores(SpeciesModel speciesModel);
+        NetModel GetRandomNetModel(SpeciesModel speciesModel);
+
+        /// <summary>
+        /// Calculates difference between two networks, to check if
+        /// they are in the same species.
+        /// </summary>
+        /// <param name="left">Network one</param>
+        /// <param name="right">Network two</param>
+        /// <returns></returns>
+        bool IsSameFamily(NetModel left, NetModel right);
+    }
+}
