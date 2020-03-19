dc92a18fd56ea8205ee853ad27f8de5ecd7341c7
Rihards Abo <denkfrt@inbox.lv>
2020-03-19 18:38:15 +0200

    Added two new models: Gnn and Species Gnn hold information about whole simulation And species contains list of same family networks

---
+++ GNNNeatLibrary/Model/GnnModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataManager.Models;
+
+namespace GNNNeatLibrary.Model
+{
+    public class GnnModel
+    {
+        public List<SpeciesModel> Species { get; set; } = new List<SpeciesModel>();
+        public List<NetModel> UnsignedNetworks { get; set; } = new List<NetModel>();
+        public BatchModel ActiveBatchModel { get; set; }
+        public double FullFitnessScore { get; set; } = 0;
+    }
+}
---
+++ GNNNeatLibrary/Model/SpeciesModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataManager.Models;
+
+namespace GNNNeatLibrary.Model
+{
+    public class SpeciesModel
+    {
+        public NetModel Head { get; set; }
+        public List<NetModel> Members { get; set; } = new List<NetModel>();
+        public double AverageFitnessScore { get; set; }
+    }
+}
