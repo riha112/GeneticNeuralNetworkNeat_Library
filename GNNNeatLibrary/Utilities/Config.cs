14af90d81d3d85d3e3db283b205bda56d986ca4f
Rihards Abo <denkfrt@inbox.lv>
2020-03-19 18:38:32 +0200

    Added static config class

---
+++ GNNNeatLibrary/Utilities/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GNNNeatLibrary.Utilities
+{
+    public static class Config
+    {
+        public static int InputLayerSize { get; set; } = 9;
+        public static int OutputLayerSize { get; set; } = 1;
+        public static double SpeciesDistanceThreshold { get; set; } = 2;
+
+        public static double MinimumFitnessScore { get; set; } = 0;
+        public static double KeepPerSpecies { get; set; } = 0.2;
+
+        public static uint NetworkCountPerPopulation { get; set; } = 10;
+
+        public static int AddRandomConnectionMutationsOnStartMin { get; set; } = 4;
+        public static int AddRandomConnectionMutationsOnStartMax { get; set; } = 9;
+    }
+}
